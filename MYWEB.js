b4f912dfcb815e5cb3322df3aa4aa86a01c136f3
lesst than renzen <69730462+freefireevogunskin@users.noreply.github.com>
2022-02-14 19:41:53 +0800

    update MYWEB.js

---
+++ MYWEB.js
@@ -0,0 +1,2 @@
+const p1 = new Image();
+p1.src="https://raw.githubusercontent.com/freefireevogunskin/freefireevogunskin.github.io/main/Screenshot_2022-02-14-19-33-02-879_com.kiwibrowser.browser.jpg";
