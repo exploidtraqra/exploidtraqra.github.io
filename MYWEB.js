0fe49d40198947299ee19c203d1ee06895cb79f3
lesst than renzen <69730462+freefireevogunskin@users.noreply.github.com>
2022-02-14 20:22:42 +0800

    update MYWEB.js

--- MYWEB.js
+++
@@ -1,4 +0,0 @@
-const slide1 = document.getElementById("slide1").getContext("2d");
-const p1 = new Image();
-p1.src="https://raw.githubusercontent.com/freefireevogunskin/freefireevogunskin.github.io/main/Screenshot_2022-02-14-19-33-02-879_com.kiwibrowser.browser.jpg";
-window.alert("ok");
