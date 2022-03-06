f382b40e16720c0457f75b919ff51fed5f014dee
lesst than renzen <69730462+exploidtraqra@users.noreply.github.com>
2022-03-06 22:22:38 +0800

    update anime.js

---
+++ anime.js
@@ -0,0 +1,74 @@
+const E1 = document.querySelector(".E1");
+const E2 = document.querySelector(".E2");
+const E3 = document.querySelector(".E3");
+const E4 = document.querySelector(".E4");
+const E5 = document.querySelector(".E5");
+const title = document.querySelector(".title");
+const video = document.querySelector(".video");
+
+class Mysource{
+	ep1(){
+		video.src="https://rr8---sn-2uuxa3vh-02bz.googlevideo.com/videoplayback?expire=1646601940&ei=VLYkYpGzDNPCkgb3lYL4Bg&ip=2a04:3543:1000:2310:30da:13ff:fead:6be6&id=7cffb1ce3f66a8b3&itag=18&source=blogger&susc=bl&mime=video/mp4&vprv=1&dur=1423.592&lmt=1560021763175296&sparams=expire,ei,ip,id,itag,source,susc,mime,vprv,dur,lmt&sig=AOq0QJ8wRAIgJ8ULeYp4ZIvTeBBrcbjNbSoxp974qldnNbuzbhNhDasCID-b3FOai2JPDIwbuyhGZsvgKBtIqtwZG99Pb_jNkVN7&redirect_counter=1&rm=sn-nposy7s&req_id=35e60cc8b63ba3ee&cms_redirect=yes&cmsv=e&ipbypass=yes&mh=W2&mip=125.167.112.128&mm=31&mn=sn-2uuxa3vh-02bz&ms=au&mt=1646573100&mv=m&mvi=8&pl=21&lsparams=ipbypass,mh,mip,mm,mn,ms,mv,mvi,pl&lsig=AG3C_xAwRgIhAM-imThef1QRwpQieALX5NKX7jhMy8MyBmDAycp4HJpSAiEAv7cbPeuRxNMh28cO8EcO5O8zqRNJ4naPoqZXnEyKtXo%3D";
+		title.innerHTML="EPISODE 1";
+	}
+	ep2(){
+		video.src="https://rr2---sn-npoe7n7s.googlevideo.com/videoplayback?expire=1646602725&ei=ZbkkYvKlG8yVkgak66-QCA&ip=2a04:3543:1000:2310:30da:13ff:fead:6be6&id=e2ab8aa5021df373&itag=18&source=blogger&mh=dd&mm=31&mn=sn-npoe7n7s&ms=au&mv=m&mvi=2&pl=32&susc=bl&mime=video/mp4&vprv=1&dur=1422.570&lmt=1560022150754978&mt=1646573575&sparams=expire,ei,ip,id,itag,source,susc,mime,vprv,dur,lmt&sig=AOq0QJ8wRgIhAPYM-fTIrYh9G3Vqjm4yNWlNCdb5REyVRmo_RddLXOYxAiEA3bO2Bmi0GZW2ibME_vAC9cDHh_Jj9GOM8jznXT4XYgY%3D&lsparams=mh,mm,mn,ms,mv,mvi,pl&lsig=AG3C_xAwRAIgbdFRyE4w4Vf8T6lPg4htWrPqFiwAyUGvCjQ_P2u6hdACIGGwWS5VGHkdCWX-nexULelqNWvw7JsHKXbZUx8lQbh9";
+		title.innerHTML="EPISODE 2";
+	}
+	ep3(){
+		video.src="https://www67.zippyshare.com/d/kTS08kbu/48786/Otakudesu_TkyRaven--03_480p.mp4";
+		title.innerHTML="EPISODE 3";
+	}
+	ep4(){
+		video.src="https://rr2---sn-npoe7nes.googlevideo.com/videoplayback?expire=1646602860&ei=7LkkYt7jH6SXn88Px--R0AE&ip=2a04:3543:1000:2310:30da:13ff:fead:6be6&id=2a334f430c1fae9d&itag=18&source=blogger&mh=9_&mm=31&mn=sn-npoe7nes&ms=au&mv=m&mvi=2&pl=32&susc=bl&mime=video/mp4&vprv=1&dur=1423.592&lmt=1560021809079304&mt=1646573575&sparams=expire,ei,ip,id,itag,source,susc,mime,vprv,dur,lmt&sig=AOq0QJ8wRQIgWVSIJo_iSkzHzmQZ5llzQ8ni9KmIVp_CBLsLwUwpvYwCIQCqSCkcxqKP_UrjcQJJGSdw7hXJyXIE_ohcMvgGHn6TOg%3D%3D&lsparams=mh,mm,mn,ms,mv,mvi,pl&lsig=AG3C_xAwRQIgeauWwHzYXmD1l_dtLoKfgXq-gczpofCrIzfYXww1GEcCIQCmeN34Slse5u3QBt07HTXmfqA7RSeAZRHdSv6DVYiauA%3D%3D";
+		title.innerHTML="EPISODE 4";
+	}
+	ep5(){
+		video.src="https://rr5---sn-2uuxa3vh-02bs.googlevideo.com/videoplayback?expire=1646603009&ei=gbokYqrcNJKKn88P5viYsAo&ip=2a04:3543:1000:2310:30da:13ff:fead:6be6&id=0d88091af54d9cb9&itag=18&source=blogger&susc=bl&mime=video/mp4&vprv=1&dur=1422.570&lmt=1560022200171209&sparams=expire,ei,ip,id,itag,source,susc,mime,vprv,dur,lmt&sig=AOq0QJ8wRQIgabxmbfZAnIb1uSx97OLwzmwyW89MOVivNAP7hHXB8bYCIQC_hsudB6S2HfSE_dFa-IPHYadiZb9ZtFM-Pq6UGvSd3w%3D%3D&redirect_counter=1&rm=sn-npoek76&req_id=ef5cc6875146a3ee&cms_redirect=yes&cmsv=e&ipbypass=yes&mh=Gc&mip=125.167.112.128&mm=31&mn=sn-2uuxa3vh-02bs&ms=au&mt=1646574048&mv=m&mvi=5&pl=21&lsparams=ipbypass,mh,mip,mm,mn,ms,mv,mvi,pl&lsig=AG3C_xAwRgIhAJxX54Lcnd7BiwJUourbewyDLX0Amgg7jtnbAKOPqNAsAiEAquntbFyMAZyZpcimjrisI13-WEZWTm-DxVd4QDQakRY%3D";
+		title.innerHTML="EPISODE 5";
+	}
+}
+
+const F = new Mysource();
+
+
+E1.addEventListener("click",function(){
+	F.ep1();
+	E1.style.background="#00A8FF";
+	E2.style.background="black";
+	E3.style.background="black";
+    E4.style.background="black";
+    E5.style.background="black";
+});
+E2.addEventListener("click",function(){
+	E2.style.background="#00A8FF";
+	E1.style.background="black";
+	E3.style.background="black";
+    E4.style.background="black";
+    E5.style.background="black";
+	F.ep2();
+});
+E3.addEventListener("click",function(){
+	E3.style.background="#00A8FF";
+	E2.style.background="black";
+	E1.style.background="black";
+    E4.style.background="black";
+    E5.style.background="black";
+	F.ep3();
+});
+E4.addEventListener("click",function(){
+	E4.style.background="#00A8FF";
+	E2.style.background="black";
+	E3.style.background="black";
+    E1.style.background="black";
+    E5.style.background="black";
+	F.ep4();
+});
+E5.addEventListener("click",function(){
+	E5.style.background="#00A8FF";
+	E2.style.background="black";
+	E3.style.background="black";
+    E4.style.background="black";
+    E1.style.background="black";
+	F.ep5();
+});
